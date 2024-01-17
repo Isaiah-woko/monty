5d824641f9a62ab5d41b462b508d20712f60d215
be-great <begreat5333@gmail.com>
2024-01-17 23:19:25 +0300

    testing comments

--- function_2.c
+++ function_2.c
@@ -9,24 +9,24 @@
 
 void monty_pchar(stack_t **stack, unsigned int line_number)
 {
-	int num = 0;
-
-	if (!*stack || !stack)
+	if (*stack != NULL && stack != NULL)
 	{
-		fprintf(stderr, "L%d: can't pchar, stack empty\n", line_number);
+		if ((*stack)->n >= 0 && (*stack)->n <= 127)
+			printf("%c\n", (*stack)->n);
+		else
+		{
+			fprintf(stderr, "L%u: can't pchar, value out of range\n",
+					line_number);
+			free_all_located();
+			exit(EXIT_FAILURE);
+		}
+	}
+	else
+	{
+		fprintf(stderr, "L%u: can't pchar, stack empty\n", line_number);
 		free_all_located();
 		exit(EXIT_FAILURE);
 	}
-
-	num = (*stack)->n;
-	if (num < 0 || num > 127)
-	{
-		fprintf(stderr, "L%d: can't pchar, value out of range\n", line_number);
-		free_all_located();
-		exit(EXIT_FAILURE);
-	}
-	putchar(num);
-	putchar(10);
 }
 /**
 * monty_pstr - The opcode pchar prints the char at the top of the stack,
--- monty_main.c
+++ monty_main.c
@@ -56,9 +56,6 @@ int main(int argc, char **argv)
 	{
 		line_number++;
 		saved_struct->line = line, saved_struct->file = file;
-		/*-----------if the line is comment*/
-		if (line[0] == '#')
-			continue;
 		if (is_empty_or_whitespace(line))
 		{
 			continue;
